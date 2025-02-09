2e76cbc072479de1e0bbe7cade59fbe0be7cc7ce
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-09 19:27:25 +0800

    Update MultiTextBox.lua

--- MultiTextBox.lua
+++ MultiTextBox.lua
@@ -17,11 +17,20 @@ function desc_from_rows(desc_nodes, empty, maxw, _c)
     if type(loc_target.boxes) ~= 'table' then return DFR(desc_nodes, empty, maxw) end
 
     local total_line_numbers = 0
+    local box_contains_only_numbers = true
     for _, box_line_number in ipairs(loc_target.boxes) do
-        total_line_numbers = total_line_numbers + box_line_number
+        if type(box_line_number) ~= 'number' then
+            box_contains_only_numbers = false
+        else
+            total_line_numbers = total_line_numbers + box_line_number
+        end
+    end
+    if not box_contains_only_numbers then
+        sendDebugMessage("Warning: .boxes contains non-number value! Card.key = ".._c.key)
+        return DFR(desc_nodes, empty, maxw)
     end
     if total_line_numbers ~= #loc_target.text then
-        sendDebugMessage("Warning: Sum of ".._c.key..".loc_txt.boxes does not match with #".._c.key..".loc_txt.text!")
+        sendDebugMessage("Warning: Sum of .boxes does not match with the total number of lines in .text! Card.key = ".._c.key)
         return DFR(desc_nodes, empty, maxw)
     end
 
