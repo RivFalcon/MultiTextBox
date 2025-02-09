58aabef0e7b3313d8e4ad7e1890452860e9d9696
Riv_Falcon <26927165+RivFalcon@users.noreply.github.com>
2025-02-09 17:47:43 +0800

    Initial commit

---
+++ MultiTextBox.lua
@@ -0,0 +1,64 @@
+----
+SMODS.Atlas({
+    key = "modicon",
+    path = "modicon.png",
+    px = 32,
+    py = 32
+})
+
+local DFR = desc_from_rows
+function desc_from_rows(desc_nodes, empty, maxw, _c)
+    if _c == nil then return DFR(desc_nodes, empty, maxw) end
+    if _c.ability_UIBox_table.card_type == 'Locked' then return DFR(desc_nodes, empty, maxw) end
+    if _c.ability_UIBox_table.card_type == 'Undiscovered' then return DFR(desc_nodes, empty, maxw) end
+    if type(desc_nodes) ~= 'table' then return DFR(desc_nodes, empty, maxw) end
+    if _c.config.center.loc_txt == nil then return DFR(desc_nodes, empty, maxw) end
+    if type(_c.config.center.loc_txt.boxes) ~= 'table' then return DFR(desc_nodes, empty, maxw) end
+
+    local total_line_numbers = 0
+    for _, box_line_number in ipairs(_c.config.center.loc_txt.boxes) do
+        total_line_numbers = total_line_numbers + box_line_number
+    end
+    if total_line_numbers ~= #_c.config.center.loc_txt.text then
+        sendDebugMessage("Warning: Sum of ".._c.key..".loc_txt.boxes does not match with #".._c.key..".loc_txt.text!")
+        return DFR(desc_nodes, empty, maxw)
+    end
+
+    local box = {}
+    local last_line_number = 0
+    for _, box_line_number in ipairs(_c.config.center.loc_txt.boxes) do
+        local t = {}
+        for current_subline_number=1, box_line_number do
+            t[#t+1] = {
+                n=G.UIT.R,
+                config={
+                    align = "cm"
+                },
+                nodes = desc_nodes[last_line_number + current_subline_number]
+            }
+        end
+        box[#box+1]={
+            n=G.UIT.R,
+            config={
+                align = "cm",
+                colour = G.C.UI.BACKGROUND_WHITE,
+                r = 0.05,
+                padding = 0.03
+            },
+            nodes=t
+        }
+        last_line_number = last_line_number + box_line_number
+    end
+    return {
+        n=G.UIT.R,
+        config={
+            align = "cm",
+            padding = 0.04,
+            minh = 0.8,
+            emboss = not empty and 0.05 or nil,
+            filler = true
+        },
+        nodes=box
+    }
+end
+----
---
+++ README.md
@@ -0,0 +1,2 @@
+# MultiTextBox
+ A Balatro UI mod where you can separate massive description texts into multiple boxes.
